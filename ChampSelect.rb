24a1b9654684fc12961456f7b1f2fc6da2fc6fc2
jhorowitz16 <joshua.horowitz@berkeley.edu>
2017-05-22 23:28:38 -0700

    requested list of champ ids, and built CSV with id, name, and title for each

--- ChampSelect.rb
+++ ChampSelect.rb
@@ -1,44 +1,7 @@
 require 'httparty'
 require_relative "helpers"
 
-
-# get a valid integer to search
-print " Please enter a champion ID: "
-id_request = gets.to_i
-while id_request <= 0
-  puts "Invalid ID. Please enter a champion ID: "
-  id_request = gets.to_i
-end
-
-
-ids = [id_request]
 api_key = get_api_key()
-
-
-# for each id, create a file for that champ
-# post xayah/rakan there are 136 champs
-errors = []
-for id in ids
-  champ_url = "https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion/" + id.to_s
-  champ_url += "/?api_key=" + api_key
-  response = HTTParty.get(champ_url)
-  case response.code
-    when 200
-      response_hash = response.parsed_response
-      name = response_hash["name"]
-      title = response_hash["title"]
-      puts id.to_s + ": " + name + ", " + title
-      filename = "champs/" + name + ".txt"
-      writer(filename, title + " >>> " + id.to_s)
-    when 404
-      puts "error 404"
-      errors += [id]
-  end
-end
-
-
-# record the ids that didn't get a response
-writer("errors.txt", errors.to_s)
-puts " ==== end ===="
-
-
+ids = get_ids(api_key)
+data = collect_data(api_key, ids)
+write_champ_data("champs.csv", data)
--- helpers.rb
+++ helpers.rb
@@ -4,23 +4,34 @@ def writer(filename, text)
 end
 
 
-# get the ids
-def get_ids()
+# get the ids (hashes with ids)
+def get_ids(api_key)
+  puts "start here"
   ids_url = "https://na.api.pvp.net/api/lol/na/v1.2/champion?api_key=" + api_key
   response = HTTParty.get(ids_url)
-  ids = []
+  ids = [13]
   case response.code
     when 200
       response_hash = response.parsed_response
       champions = response_hash["champions"]
       puts champions.class
       champions.each do |champ|
-        puts champ.to_s
         ids += [champ["id"]]
       end
     when 404
       puts "404 - could not find champion list"
+  end
   return ids
+end
+
+
+# write the champ data to a csv
+def write_champ_data(filename, data)
+  CSV.open(filename, "wb") do |csv|
+    csv << ["ID", "Name", "Title"]
+    data.each do |data|
+      csv << data
+    end
   end
 end
 
@@ -35,3 +46,38 @@ def get_api_key()
 end
 
 
+# for each champ id, get the champ name and title
+def collect_data(api_key, ids)
+  data = []
+  errors = []
+  for id in ids
+    puts "collecting data for " + id.to_s
+    champ_url = "https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion/" + id.to_s
+    champ_url += "/?api_key=" + api_key
+    response = HTTParty.get(champ_url)
+    case response.code
+      when 200
+        response_hash = response.parsed_response
+        name = response_hash["name"]
+        title = response_hash["title"]
+        data += [[id, name, title]]
+      when 404
+        puts "error 404"
+        errors += [id]
+        data += [[id, "no_name", "no_title"]]
+    end
+  end
+  return data
+end
+
+
+# get a valid integer to search
+def prompt_id_request()
+  print " Please enter a champion ID: "
+  id_request = gets.to_i
+  while id_request <= 0
+    puts "Invalid ID. Please enter a champion ID: "
+    id_request = gets.to_i
+  end
+  return id_request
+end
